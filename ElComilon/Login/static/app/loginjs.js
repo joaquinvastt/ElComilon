3e48c946f34e344a8c01994beba056dd7e10666e
BastianAhumada <bastianahumadajimenez@gmail.com>
2021-10-01 21:40:44 -0300

    1.2.2
    Validaciones js login

--- ElComilon/Login/static/app/loginjs.js
+++ ElComilon/Login/static/app/loginjs.js
@@ -1,4 +1,3 @@
-
 let Botonsiguiente = document.getElementById("btn-siguiente")
 const inputNombreusuario = document.getElementById("nameUser")
 const inputPassword = document.getElementById("passwordUser")
@@ -11,33 +10,57 @@ const expresiones = {
 }
 const Validarcampo = (e) =>
 {
-  console.log(inputNombreusuario.value)
-  console.log(expresiones.usuario.test(inputNombreusuario))
-  if(expresiones.usuario.test(inputNombreusuario)){
-    txt_error.classList.replace('TxtERROR','txtp')
-    inputNombreusuario.classList.replace('inputinvalido','inputvalido')
-    console.log("campo usuario correcto") 
-  }
-  else{ 
+if(inputNombreusuario.value.length < 8){
     inputNombreusuario.classList.add('inputinvalido')
-    txt_error.classList.replace('txtp','TxtERROR')
-    console.log("campo usuario incorrecto") 
+    txt_error.classList.remove('txtp')
+    txt_error.classList.add('TxtERROR')
+    txt_error.innerHTML = "El nombre de usuario debe ser minimo 8 caracteres" 
+    console.log("campos vacio") 
+    
   }
-  console.log(expresiones.password.test(inputPassword))
-  if(expresiones.password.test(inputPassword)){
-    console.log("campo password correcto")
-  }
-  else{ 
-    inputPassword.classList.add('inputinvalido')
+    else{
+      txt_error.innerHTML = "Nombre de usuario valido" 
+      txt_error.classList.remove('TxtERROR')
+      txt_error.classList.add('txtexito')
+      inputNombreusuario.classList.remove('inputinvalido')
+      inputNombreusuario.classList.add('valido')
+     
+    }
+ if(inputPassword.value.length < 8) {
+  inputPassword.classList.add('inputinvalido')
     txt_errorpass.classList.add('TxtERROR')
-    console.log("campo password incorrecto")
-  }
+    txt_errorpass.innerText = "La contraseña debe ser minimo 8 caracteres"
+    
+ }
+    else{
+
+      txt_errorpass.innerHTML = "Contraseña valida" 
+      txt_errorpass.classList.remove('TxtERROR')
+      txt_errorpass.classList.add('txtexito')
+      inputPassword.classList.remove('inputinvalido')
+      inputPassword.classList.add('valido')
+      return
+    }
+/*
+   else {
+    inputNombreusuario.classList.replace('inputinvalido','inputvalido')
+    txt_error.classList.remove('TxtERROR')
+    txt_error.classList.add('txtp')
+
+    inputPassword.classList.replace('inputinvalido','inputvalido')
+    txt_errorpass.classList.remove('TxtERROR')
+    txt_errorpass.classList.add('txtp')
+     console.log("EXito")
+     return
+   }
+
   return
+  */
 }
 
 FormLogin.addEventListener("submit",e => {
- 
   e.preventDefault()
+  console.log("Enviando info")
   Validarcampo(e)
 }
 )
